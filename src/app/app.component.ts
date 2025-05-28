2f4bcb48c4d6a5fba5bd00a604d762fb102e2bcb
Michelle <mpuschkarow@web.de>
2025-05-28 19:17:13 +0200

    cursor component generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CursorComponent } from './shared/cursor/cursor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CursorComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
