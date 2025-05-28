a290102fae12329ec0ac771b2f7b01fa01163d00
Michelle <mpuschkarow@web.de>
2025-05-28 19:12:59 +0200

    basic structure and design for section about me

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -11,4 +11,21 @@ import { HeaderComponent } from '../../shared/header/header.component';
 })
 export class HeroComponent {
 
+// document.querySelectorAll(".bouncing-letters>span").forEach((element) => {
+//   element.addEventListener("mouseover",
+//     (e) => bounce(e.target));
+// });
+
+// function bounce(letter) {
+//   if (!letter.classList.contains("bounce")) {
+//     letter.classList.add("bounce");
+//     setTimeout(
+//       function () {
+//         letter.classList.remove("bounce");
+//       },
+//       1000
+//     );
+//   }
+// }
+
 }
