385d0680399ab38d2828ffced5106938929c093b
Michelle <mpuschkarow@web.de>
2025-05-29 15:43:14 +0200

    header lines images placed for each nav-link; toggle function to switch language image

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  english = false;
 
+  switchLanguage(){
+    if(this.english){
+      this.english = false;
+    } else {
+      this.english = true;
+    }
+  }
 }
