6b37ea7bb20e25268acf6d9c6836d1055313fe0d
Michelle <mpuschkarow@web.de>
2025-05-31 19:46:23 +0200

    new component generated for project previews

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { PreviewComponent } from './projects/preview/preview.component';
 
 export const routes: Routes = [
-    {path: '', component: MainContentComponent}
+    {path: '', component: MainContentComponent},
+    {path: 'preview', component: PreviewComponent},
+   
 ];
