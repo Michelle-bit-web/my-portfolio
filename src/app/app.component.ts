0b1bf220bab4552c5602109ea6792c3dd977a418
Michelle <mpuschkarow@web.de>
2025-06-02 09:58:14 +0200

    json files added for translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CursorComponent } from './shared/cursor/cursor.component';
 import { TranslateModule } from '@ngx-translate/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -16,4 +17,15 @@ import { TranslateModule } from '@ngx-translate/core';
 export class AppComponent {
   title = 'my-portfolio';
 
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+    // this.translate.use(this.translate.getBrowserLang() || "en");
+  }
+
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+}
 }
