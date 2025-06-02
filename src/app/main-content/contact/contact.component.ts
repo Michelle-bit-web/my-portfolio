2037ed3b5c19e9e9f1a2030c80ea1be111b73712
Michelle <mpuschkarow@web.de>
2025-06-02 17:16:50 +0200

    nav links connected to sections

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
@@ -18,7 +18,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  http = inject(HttpClient)
   contactData = {
     name: '',
     email: '',
@@ -26,10 +26,35 @@ export class ContactComponent {
     checkedStatus: false,
   }
 
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    console.log(ngForm)
-    if(ngForm.valid && ngForm.submitted)
-    console.log('form submitted', this.contactData);
-    
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            //hier könnte man auch weiteres ergänzen, was passieren soll
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'), //die consolen-Info könnte man entfernen am Ende
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      //Wenn man oben noch was ergänzt, muss das hier auch hin?
+      ngForm.resetForm();
+    }
   }
 }
