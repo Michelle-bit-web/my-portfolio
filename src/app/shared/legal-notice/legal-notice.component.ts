fe7c469d691296b5dd324bb6e0c6f5d8487020df
Michelle <mpuschkarow@web.de>
2025-06-07 11:15:13 +0200

    translations links  for legal notice and privacy police added to html

--- src/app/shared/legal-notice/legal-notice.component.ts
+++ src/app/shared/legal-notice/legal-notice.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../../main-content/contact/footer/footer.component';
 import { RouterLink, RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
@@ -9,7 +10,9 @@ import { HeaderComponent } from '../header/header.component';
 
     FooterComponent,
     RouterModule,
-    RouterLink
+    RouterLink,
+    TranslateModule,
+    TranslatePipe
   ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
--- src/app/shared/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../main-content/contact/footer/footer.component';
 import { RouterLink, RouterModule } from '@angular/router';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
   imports: [
     FooterComponent,
     RouterModule,
-    RouterLink
+    RouterLink,
+    TranslateModule,
+    TranslatePipe
+
   ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
